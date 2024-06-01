afad93f4985d4999b940143922f3d8f3666d3a72
skyhaibara <145948549+skyhaibara@users.noreply.github.com>
2024-06-01 13:16:29 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -16,13 +16,13 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>welcome to 灰原哀 website</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          QQ：328830204
         </p>
       </div>
       <p className="read-the-docs">
