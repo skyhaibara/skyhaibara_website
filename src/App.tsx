30069e2687a90278134b473d7a5ea3a28837190c
灰原哀 <haibara@stu.tzc.edu.cn>
2024-11-18 17:12:49 +0800

    update the logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import haibaraImage from './assets/fr_haibara.png';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <img className="logo" src={haibaraImage} alt="灰原哀" />
       </div>
       <h1>welcome to 灰原哀's website</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
         <p>
           QQ：328830204 wx: sky-sherry-ai .
         </p>
