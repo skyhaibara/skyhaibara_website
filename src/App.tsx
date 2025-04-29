f5c467a505c868a5297be9ce40a799111b5518b6
灰原哀 <haibara@stu.tzc.edu.cn>
2025-04-29 21:44:17 +0800

    rebuild with pnpm

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,33 @@
-import haibaraImage from './assets/fr_haibara.png';
-import Button from '@mui/material/Button';
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
     <>
       <div>
-      <img className="logo" src={haibaraImage} alt="灰原哀" />
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
       </div>
-      <h1>welcome to 灰原哀's website</h1>
+      <h1>Vite + React</h1>
       <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
         <p>
-          QQ：328830204 wx: sky-sherry-ai .
+          Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
-        try to follow my github account to know more about skyhaibara.
+        Click on the Vite and React logos to learn more
       </p>
-        <Button variant="contained">
-          Hello world
-        </Button>
     </>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
 import './index.css'
+import App from './App.tsx'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
     <App />
-  </React.StrictMode>,
+  </StrictMode>,
 )
-
-import Button from '@mui/material/Button';
-
-export default function ButtonUsage() {
-  return <Button variant="contained">Hello world</Button>;
-}
